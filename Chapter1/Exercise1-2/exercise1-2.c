9a319b32778a0d7736c7c9920a2879ea65952f9f
PMiranda <pedro_miranda2@hotmail.com>
2021-08-03 19:15:50 +0100

    Added Exercises 1-1 and 1-2
    Exercise 1-2 still needs to be complete

---
+++ Chapter1/Exercise1-2/exercise1-2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    printf("Exercise 1-2\n");
+    printf("\n\n\n");
+    printf("\a");
+    printf("\b");
+    printf("\c");
+    printf("\d");
+    printf("\e");
+    printf("\f");
+    printf("\g");
+    printf("\h");
+    printf("\i");
+    printf("\j");
+    printf("\k");
+    printf("\l");
+    printf("\m");
+    printf("\n");
+    printf("\o");
+    printf("\p");
+    printf("\q");
+    printf("\r");
+    printf("\s");
+    printf("\t");
+    printf("\u");
+    printf("\v");
+    printf("\w");
+    printf("\x");
+    printf("\y");
+    printf("\z");
+
+
+    /* TODO */
+    /* Include all valid printf escape sequences */
+}
