b4979e7ffc88d143799d7afaae3641c7f46b5880
PMiranda <pedro_miranda2@hotmail.com>
2021-08-04 08:10:34 +0100

    Added Exercise description. Completed Exercise1-2

--- Chapter1/Exercise1-1/exercise1-1.c
+++ Chapter1/Exercise1-1/exercise1-1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ * Exercise 1-1: Run the "Hello, world" program on your system. Experiment with
+ * leaving out parts of the program, to see what error messages you get.
+ * */
+
 int main(){
     printf("Hello ");
     printf("world");
--- Chapter1/Exercise1-2/exercise1-2.c
+++ Chapter1/Exercise1-2/exercise1-2.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
+/* 
+ * Exercise 1-2: Experiment to find out what happens when printf's argument
+ * string contains \c, where c is some character not listed above.
+ * (Listed above: \n, \t, \b, \\)
+ * */
 int main(){
     printf("Exercise 1-2\n");
     printf("\n\n\n");
-    printf("\a");
-    printf("\b");
-    printf("\c");
-    printf("\d");
-    printf("\e");
-    printf("\f");
-    printf("\g");
-    printf("\h");
-    printf("\i");
-    printf("\j");
-    printf("\k");
-    printf("\l");
-    printf("\m");
-    printf("\n");
-    printf("\o");
-    printf("\p");
-    printf("\q");
-    printf("\r");
-    printf("\s");
-    printf("\t");
-    printf("\u");
-    printf("\v");
-    printf("\w");
-    printf("\x");
-    printf("\y");
-    printf("\z");
-
-
-    /* TODO */
-    /* Include all valid printf escape sequences */
+    /* valid printf escape sequences */
+    printf("\a");           /* Alert (Beep, bell)*/
+    printf("\b");           /* Backspace */
+    printf("\e");           /* Escape character */
+    printf("\f");           /* Formfeed Page Break */
+    printf("\n");           /* Newline */
+    printf("\r");           /* Carriage Return */
+    printf("\t");           /* Horizontal Tab */
+    printf("\v");           /* Vertical Tab */
+    printf("\\");           /* Backslash */
+    printf("\'");           /* Apostrophe / single quotation */
+    printf("\"");           /* Double quotation */
+    printf("\?");           /* Question mark */
+    printf("\123");         /* Byte in octal form */
+    printf("\xFF");         /* Byte in hexadecimal form */
+    printf("\uFFFF");       /* Unicode bellow 10000 hexadecimal */
+    printf("\uFFFFFFFF");   /* Unicode in hexadecimal */
+    
 }
