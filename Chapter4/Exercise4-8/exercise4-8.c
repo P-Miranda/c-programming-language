1df4dac70ba4829b20a9836d0330dc09de9e4618
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2021-09-19 10:25:00 +0100

    fix: Exercise4-8
    Change buffer to a single character

--- Chapter4/Exercise4-8/exercise4-8.c
+++ Chapter4/Exercise4-8/exercise4-8.c
@@ -4,10 +4,11 @@
 
 #define MAXOP 100 /* max size of operand or operator */
 #define NUMBER '0' /* signal that a number was found */
-#define VARIABLE 'A' /* signal that a variable was found */
 
 /* Suppose that there will never be more than one character of pushback. Modify
  * `getch` and `ungetch` accordingly. */
+
+/* character buffer can be a single char */
 
 /*
  * - print top element ?
@@ -35,26 +36,16 @@ void clear_stack(void);
 void push(double);
 double pop(void);
 
-void variable_init(void);
-void variable_operation(char s[]);
-void variable_store_last_printed(double value);
-
-int strlen_(char s[]);
 /* reverse Polish calculator */
 int main(){
     int type;
     double op2, op1, res;
     char s[MAXOP];
 
-    variable_init();
-
     while((type = getop(s)) != EOF){
         switch(type){
             case NUMBER:
                 push(atof(s));
-                break;
-            case VARIABLE:
-                variable_operation(s);
                 break;
             case '+':
                 push(pop() + pop());
@@ -109,9 +100,7 @@ int main(){
                 clear_stack();
                 break;
             case '\n':
-                op1 = pop();
-                variable_store_last_printed(op1);
-                printf("\t%.8g\n", op1);
+                printf("\t%.8g\n", pop());
                 break;
             default:
                 printf("error: unknown command %s\n", s);
@@ -150,7 +139,6 @@ void print_top(void){
     if(sp > 0) {
         value = val[sp-1];
         printf("Stack top: %f\n", value);
-        variable_store_last_printed(value);
     } else
         printf("Stack top: <empty>\n");
     return;
@@ -187,8 +175,6 @@ void clear_stack(void){
 
 int getch(void);
 void ungetch(int);
-void ungets(char s[]);
-int is_capital_letter(char c);
 
 /* getop: get next operator or numeric operand */
 int getop(char s[]){
@@ -198,9 +184,6 @@ int getop(char s[]){
         ;
     s[1] = '\0';
     
-    if(is_capital_letter(c)) /* check for variable */
-        return VARIABLE;
-
     if(!isdigit(c) && c != '.')
         return c; /* not a number */
     
@@ -221,113 +204,24 @@ int getop(char s[]){
     return NUMBER;
 }
 
-int is_capital_letter(char c){
-    return (c >= 'A' && c <= 'Z');
-}
 
-#define BUFSIZE 100
-
-char buf[BUFSIZE]; /* buffer for ungetch */
-int bufp = 0; /* next free position in buf */
+char buf; /* buffer for ungetch only a single char */
+int bufp = 0; /* (0) buffer free, (1) buffer has char */
 
 int getch(void){ /* get a (possibly pushed back) character */
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    if(bufp){ 
+        bufp--;
+        return buf;
+    } else {
+        return getchar();
+    }
 }
 
 void ungetch(int c){ /* push character back on input */
-    if(bufp >= BUFSIZE)
-        printf("ungetch: too many characters\n");
-    else
-        buf[bufp++] = c;
-}
-
-void ungets(char s[]){ /* push string back on input */
-    int len = strlen_(s);
-    int i = 0;
-    if(bufp + len >= BUFSIZE)
-        printf("ungets: string does not fit buffer\n");
-    else
-        for(i = 0; i < len; i++)
-            buf[bufp++] = s[i];
-}
-
-/* Variable operations */
-#define LAST_PRINTED 'P' /* name for variable to store last printed value */
-#define N_VARIABLES ('Z'-'A'+1) /* number of variables */
-
-double variable_array[N_VARIABLES]; /* structure with variables */
-
-double variable_get(int var);
-void variable_store_stack_top(int var);
-void variable_store_value(int var, double value);
-
-void variable_init(void){
-    int i = 0;
-    for(i=0; i< N_VARIABLES; i++)
-        variable_array[i] = 0.0;
-    return;
-}
-
-/* if var == LAST_PRINTED print last printed value
- * if var == 0: save top of stack to variable
- * if var != 0: print variable
- * */
-void variable_operation(char s[]){
-    int var = s[0];
-    switch(var){
-        case LAST_PRINTED:
-            printf("Last printed: %f\n", variable_get(LAST_PRINTED));
-            break;
-        default:
-            printf("var default\n");
-            if(variable_get(var) == 0.0){
-                variable_store_stack_top(var);
-            } else {
-                printf("%c: %f\n",(char) var, variable_get(var));
-            }
-            break;
+    if(bufp)
+        printf("ungetch: there is already a buffered character\n");
+    else{
+        buf = c;
+        bufp++;
     }
-    return; 
-}
-
-void variable_store_stack_top(int var){
-    int idx = var - VARIABLE;
-    double value = 0;
-    if(idx >= 0 && idx < N_VARIABLES){
-        value = pop();
-        push(value);
-        variable_array[idx] = value;
-    } else
-        printf("Error: invalid variable\n");
-    return;
-}
-
-void variable_store_value(int var, double value){
-    int idx = var - VARIABLE;
-    if(idx >= 0 && idx < N_VARIABLES){
-        variable_array[idx] = value;
-    } else
-        printf("Error: invalid variable\n");
-    return;
-}
-
-double variable_get(int var){
-    int idx = var - VARIABLE;
-    if(idx >= 0 && idx < N_VARIABLES){
-        return variable_array[idx];
-    } else {
-        printf("Error: invalid variable\n");
-        return 0.0;
-    }
-}
-
-void variable_store_last_printed(double value){
-    variable_store_value(LAST_PRINTED, value);
-    return;
-}
-
-int strlen_(char s[]){
-    int len = 0;
-    while(s[len++] != '\0');
-    return len;
 }
