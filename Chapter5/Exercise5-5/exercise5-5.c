67ced8160caa9a2a1b2f350f20e77b3e8d94e9dd
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2021-10-06 07:48:20 +0100

    feat: Finished Exercise5-5

--- Chapter5/Exercise5-5/exercise5-5.c
+++ Chapter5/Exercise5-5/exercise5-5.c
@@ -21,18 +21,18 @@ int main(){
     char s_c[] = "aaaaZZZZ";
 
     /* strncpy */
-    printf("strncpy_(): %s\n", strncpy_(s_cpy, t, 3));
-    printf("strncpy_(): %s\n", strncpy_(s_cpy, t, 30));
+    printf("strncpy_(%s, %s, %d):\n\t%s\n", s_cpy, t, 3, strncpy_(s_cpy, t, 3));
+    printf("strncpy_(%s, %s, %d):\n\t%s\n", s_cpy, t, 30, strncpy_(s_cpy, t, 30));
 
-    /* /1* strncat *1/ */
-    /* printf("strncat_(): %s\n", strncat_(s, t, 4)); */
-    /* printf("strncat_(): %s\n", strncat_(s, t, 20)); */
+    /* strncat */
+    printf("strncat_(%s, %s, %d):\n\t%s\n", s, t, 4, strncat_(s, t, 4));
+    printf("strncat_(%s, %s, %d):\n\t%s\n", s, t, 20, strncat_(s, t, 20));
 
-    /* /1* s != t and s >= t *1/ */
-    /* printf("strncmp_(%s, %s, %d): %d\n",s_a, s_b, 4, strncmp_(s_a, s_b, 4)); */
-    /* printf("strncmp_(%s, %s, %d): %d\n",s_a, s_b, 10, strncmp_(s_a, s_b, 10)); */
-    /* printf("strncmp_(%s, %s, %d): %d\n",s_a, s_c, 4, strncmp_(s_a, s_c, 4)); */
-    /* printf("strncmp_(%s, %s, %d): %d\n",s_a, s_c, 10, strncmp_(s_a, s_c, 10)); */
+    /* s != t and s >= t */
+    printf("strncmp_(%s, %s, %d): %d\n",s_a, s_b, 4, strncmp_(s_a, s_b, 4));
+    printf("strncmp_(%s, %s, %d): %d\n",s_a, s_b, 10, strncmp_(s_a, s_b, 10));
+    printf("strncmp_(%s, %s, %d): %d\n",s_a, s_c, 4, strncmp_(s_a, s_c, 4));
+    printf("strncmp_(%s, %s, %d): %d\n",s_a, s_c, 10, strncmp_(s_a, s_c, 10));
 
     return 0;
 }
@@ -43,23 +43,14 @@ int main(){
 char * strncpy_(char *s, char *t, int n){
     char *s_aux = s;
     int i = 0;
-    printf("strcpy start\n");
 
     for(i=0; t[i]!='\0' && i<n; i++){
         s[i] = t[i];
-        printf("%d: %s\n", i, s);
     }
 
-    /* for(; (*s_aux++ = *t++) && n; n--){ */
-    /*     printf("%d: %s\n", n, s); */
-    /* } */
-    
-    printf("strcpy after copy: %s\n", s);
     while(i<n){
         s[i++] = '\0';
     }
-
-    printf("strcpy terminate string\n");
 
     return s;
 }
@@ -67,23 +58,27 @@ char * strncpy_(char *s, char *t, int n){
 /* strncat_(): concatenate at most n characters of string t to end of s, 
  * terminate s with '\0's, return s. */
 char * strncat_(char *s, char *t, int n){
+    char *s_aux = s;
     /* go to s end */
-    while(*s != '\0')
-        s++;
+    while(*s_aux != '\0')
+        s_aux++;
     /* copy t after s */
-    while((*s++ = *t++) && n--)
+    while((*s_aux++ = *t++) && n--)
         ;
     /* terminate string */
-    *s = '\0';
+    *s_aux = '\0';
     return s;
 }
 
 /* strncmp_(): compare at most n characters of string t to string s; return <0 if
  * s<t, 0 if s==t, or >0 if s>t. */
 int strncmp_(char *s, char *t, int n){
-    for(; (*s == *t) && n; s++, t++, n--)
-        if(*s == '\0')
+    for(; (*s == *t) && (n>0); s++, t++, n--){
+        if((*s == '\0') || (*t == '\0'))
             return 0;
+        if(n==1)
+            return *s - *t;
+    }
     return *s - *t;
 }
 
